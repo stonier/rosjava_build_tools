325f506d29d3e4e3a4e39441104ca31c1fd238b4
Daniel Stonier <d.stonier@gmail.com>
2014-02-09 15:10:59 +0900

    catkin create rosjava app and library wizards

--- setup.py
+++ setup.py
@@ -11,6 +11,7 @@ d = generate_distutils_setup(
              'scripts/catkin_create_android_library_project',
              'scripts/catkin_create_rosjava_pkg',
              'scripts/catkin_create_rosjava_project',
+             'scripts/catkin_create_rosjava_library_project',
              'scripts/catkin_create_rosjava_msg_project',
             ],
     package_data = {'rosjava_build_tools': [
@@ -19,6 +20,7 @@ d = generate_distutils_setup(
            'templates/rosjava_msg_project/*',
            'templates/rosjava_package/*',
            'templates/rosjava_project/*',
+           'templates/rosjava_library_project/*',
            'templates/init_repo/*',
         ]},
     requires=['rospy' 'rospkg']
